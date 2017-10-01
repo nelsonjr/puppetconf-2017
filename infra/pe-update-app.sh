21e0e7f0f05dfef4e21ba19f193cb1f93e077497
Nelson Araujo <nelsona@google.com>
2017-10-01 22:38:14 +0000

    Retry and login for pe-update-app

--- infra/pe-update-app.sh
+++ infra/pe-update-app.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-puppet job run --query 'resources[certname] { type = "Class" and title = "Profile::Myapp" }'
+declare -r logfile="${0}.log"
+puppet job run --query \
+    'resources[certname] { type = "Class" and title = "Profile::Myapp" }' \
+  2>&1 | tee "${logfile}"
+
+declare -r tok_miss=$(grep -c 'No credentials supplied' "${logfile}")
+declare -r tok_exp=$(grep -c 'The provided token has expired' "${logfile}")
+
+if [[ $tok_miss -gt 0 || $tok_exp -gt 0 ]]; then
+  echo 'Login required'
+  err=1
+  while [[ $err -ne 0 ]]; do
+    read -p "(puppet admin) password: " -s password
+    echo $password | puppet access login --username=admin --lifetime=1h
+    err=$?
+  done
+  $0 # run again
+fi
