f4ce29e085b9d0640e9fd394a7371177ea3f1b54
Nelson Araujo <nelsona@google.com>
2017-10-12 07:37:19 +0000

    Allow script to run from anywhere

--- zero-to-prod-in-10min/infra/scripts/pe-clean-jobs.sh
+++ zero-to-prod-in-10min/infra/scripts/pe-clean-jobs.sh
@@ -1,2 +1,3 @@
 su - pe-postgres -s /bin/bash \
-  -c "/opt/puppetlabs/server/bin/psql -f $(dirname "$0")/pe-clean-jobs.sql"
+  -c "/opt/puppetlabs/server/bin/psql \
+  -f $(dirname $(realpath "$0"))/pe-clean-jobs.sql"
